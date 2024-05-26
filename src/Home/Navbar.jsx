ee4485e8ed78ba774054f9beb6712f108e312364
Rifat Rahman <rifat.rahman378@gmail.com>
2024-05-27 01:47:50 +0600

    added search Option

--- src/Home/Navbar.jsx
+++ src/Home/Navbar.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import { ANIME } from '@consumet/extensions';
 
 const Navbar = () => {
     const [searchQuery, setSearchQuery] = useState('');
@@ -8,6 +9,12 @@ const Navbar = () => {
     };
 
     const handleSearch = () => {
+        const gogoanime = new ANIME.Gogoanime();
+        gogoanime.search(searchQuery).then(data => {
+            console.log(data);
+        }).catch(error => {
+            console.error('Error searching for anime:', error);
+        });
         console.log(`Searching for: ${searchQuery}`);
         // You can add additional logic here to handle the search action
     };
@@ -29,7 +36,9 @@ const Navbar = () => {
                         />
                     </div>
                     <button onClick={handleSearch} className="btn btn-ghost btn-circle">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                        </svg>
                     </button>
                     <div className="dropdown dropdown-end">
                         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
